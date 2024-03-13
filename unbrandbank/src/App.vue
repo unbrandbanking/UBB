041fce32c44cebcdccb35864b6715ff805869ffe
munchgjones <83802721+unbrandbanking@users.noreply.github.com>
2024-03-13 21:02:41 +0000

    kingcarter

--- unbrandbank/src/App.vue
+++ unbrandbank/src/App.vue
@@ -1,35 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import BankAccount from './components/BankAccount.vue';
+import Business from './components/Business.vue';
+import Contacts from './components/Contacts.vue';
+import CreditCard from './components/CreditCard.vue';
+import CreditLoans from './components/CreditLoans.vue';
+import Dashboard from './components/Dashboard.vue';
+import Database from './components/Database.vue';
+import Employement from './components/Employement.vue';
+import Home from './components/Home.vue';
+import Kids from './components/Kids.vue';
+import Loans from './components/Loans.vue';
+import Login from './components/Login.vue';
+import Mortgage from './components/Mortgage.vue';
+import ParentLoans from './components/ParentLoans.vue';
+import Parents from './components/Parents.vue';
+import Partners from './components/Partners.vue';
+import Send from './components/Send.vue';
+import Signup from './components/Signup.vue';
+import StudentsLoans from './components/StudentsLoans.vue';
+import Transactions from './components/Transactions.vue';
+import Transfer from './components/Transfer.vue';
+import Users from './components/Users.vue';
 </script>
 
 <template>
   <div id="app">
-    <router-view/>
+    <header>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer>
+    </footer>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
-<style>
-/* Add your global styles here */
-</style>
-
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
